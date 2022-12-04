80842a555b8a29fc66a36da73113899d97384deb
domcos223 <horvath.dominika223@gmail.com>
2022-12-04 02:43:52 +0100

    email sending works for one recipient

--- survey-app/src/app/models/emailtemplate.ts
+++ survey-app/src/app/models/emailtemplate.ts
@@ -2,10 +2,5 @@ export class EmailTemplate {
   emails: string[] = [];
   subject: string;
   content: string;
-
-  constructor(emails: string[], subject: string, content: string) {
-    this.emails = emails;
-    this.subject = subject;
-    this.content = content;
-  }
 }
+
---
+++ survey-app/src/app/shared/email-send.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { EmailComponent } from '../email/email.component';
+import { EmailTemplate } from '../models/emailtemplate';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmailSendService {
+
+  constructor(private http: HttpClient) {
+   }
+
+  formData : EmailTemplate = new EmailTemplate();
+  readonly baseURL = 'https://localhost:7012/api/Email'
+
+  sendEmail()  {
+    console.log(this.formData);
+    return this.http.post(this.baseURL, this.formData);
+  }
+}
