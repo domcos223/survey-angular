dbdaae530ab279013359deb1c8d9a598d3dfd6a8
domcos223 <horvath.dominika223@gmail.com>
2022-12-08 23:05:53 +0100

    pagination, orderid, minor issues fixed

--- survey-app/src/app/models/question.ts
+++ survey-app/src/app/models/question.ts
@@ -7,22 +7,26 @@ export class Question {
   surveyId: string;
   options: Option[] = [];
   mostAnsweredOp: string;
+  orderId: number;
 
-  constructor(id: string, surveyId: string, text: string, mostAnsweredOp: string) {
+  constructor(id: string, orderId: number, surveyId: string, text: string, mostAnsweredOp: string) {
     this.id = id;
+    this.orderId = orderId;
     this.surveyId = surveyId;
     this.text = text;
     this.mostAnsweredOp = mostAnsweredOp;
   }
 
   addOption(){
-
-    this.options.push(new Option(this.genOptionId(),this.id,"",false,0));
-
+    this.options.push(new Option(this.genOptionId(), this.genOrderId(), this.id,"",false,0));
   }
 
   genOptionId() {
     let generatedId = uuidv4();
     return generatedId;
+  }
+
+  genOrderId() {
+    return this.options.length+1;
   }
 }
